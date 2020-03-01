dcfca904fb5904bb0043cd91cfa99e36cb5e01f3
safa-dci <safa.bouhlel91@gmail.com>
2020-03-01 21:29:44 +0100

    final

--- js/index.js
+++ js/index.js
@@ -1,35 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////
+//   I MAKE A TEST FUNCTION WHICH WILL DELETE ALL THE DUPLICATE VALUES      //
+//////////////////////////////////////////////////////////////////////////////
+
 let stor = 0
-function pairs(arr){
-    let b=test(arr) 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            x='('+ arr[i]+','+arr[j]+') '
-            document.write(x+ ' ')
-            
-        }
-        stor=stor + i
-    }
-    document.write (b)
-    return stor
-}
-
-document.write('<br>The number of sorted pairs formed : '+ pairs([5,2,3,1,1])+ '<br>')
-
-
 function test(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = i+1; j < arr.length; j++) {
-                if(arr[j]==arr[i]){
-                     b='('+ arr[i]+','+arr[i]+') '
-                    arr.splice(j,1)
-                    stor=stor +1
-                    
-                } else {
-                    b=''
+    let deleteDuplicateArray=[]
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++){
+            if(arr[j]==arr[i]){
+                let z ='('+ arr[i]+','+arr[i]+') '
+                arr.splice(j,1)
+                j--
+                if (deleteDuplicateArray.indexOf(z)== -1) {
+                    deleteDuplicateArray.push(z)
+                    stor=stor + 1
                 }
-                
             }
             
         }
-    return b
+
+    }
+    return deleteDuplicateArray
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//                      PUT ALL THE PAIRS ON AN ARRAY                       //
+//////////////////////////////////////////////////////////////////////////////
+
+function pairs(arr){
+    let pairsArray=[]
+    let finalArray=test(arr) 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            pairsArray.push('('+ arr[i]+','+arr[j]+') ')
+        }
+        stor=stor + i
+    }
+    document.write (pairsArray.concat(finalArray))
+    return stor
+}
+
+document.write('<br>The number of sorted pairs formed : '+pairs([2,5,6,2,2,2,2,2])+ '<br>')
